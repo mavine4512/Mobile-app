9e058c769855a1a50b8b099453ea6cdb7d80b6a5
RodneyOnyi <onyangorodney@gmail.com>
2020-12-01 14:53:15 +0300

    Fix Icon issue

--- Components/TodoItem.js
+++ Components/TodoItem.js
@@ -1,35 +1,28 @@
 import React from 'react';
-import { StyleSheet,Text, View,TouchableOpacity } from 'react-native';
-// import { MaterialIcons } from 'react-native-vector-icons';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/dist/MaterialIcons';
 
-export default function TodoItem({item,pressHandler}){
-    
-    return(
-        <TouchableOpacity onPress={()=>pressHandler(item.id)}>
-           <View style={styles.item}>
-               {/* <MaterialIcons name='delete' size={12} color='#333'/> */}
-               <Text style={styles.itemText}>
-                  {item.name}
-                </Text>
-           </View>
-        </TouchableOpacity>
-    )
+export default function TodoItem({item, pressHandler}) {
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.id)}>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={12} color="#333" />
+        <Text style={styles.itemText}>{item.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
-const styles=StyleSheet.create({
-    item:{
-        padding:16,
-        marginTop:16,
-        flexDirection:'row',
-        borderColor:'#bbb',
-        borderWidth:1,
-        borderStyle:'dashed',
-        borderRadius:12,
-    },
-    itemText:{
-        marginLeft:10
-        
-    }
-
-})
-
-
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    marginTop: 16,
+    flexDirection: 'row',
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 12,
+  },
+  itemText: {
+    marginLeft: 10,
+  },
+});
